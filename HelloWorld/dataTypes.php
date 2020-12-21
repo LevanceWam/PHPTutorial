173e2954ac3540dc94f7e0357598b65dc4ac15a3
LevanceWam <lkwamley@fullsail.edu>
2020-12-21 18:31:41 -0500

    Learned about data types in php

---
+++ HelloWorld/dataTypes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Types</title>
+</head>
+<body>
+    <!-- Strings: plain text have to use ",'-->
+
+    <!-- Numbers: we have 2 types whole numbers and decimal numbers. -->
+    <!-- We refer to whole numbers as intergers integers can aslo be negative -->
+    <!-- we refer to decimals as floats -->
+    <!-- it is important to Distinguish between the 2 PHP  -->
+
+    <!-- boolean: true or false -->
+
+    <!-- Null this stands for no value can be seen on a error value and some places -->
+    <?php
+        $phrase = "To be or not to be";
+        // there is a different between these 2
+        $age = 12;
+        $gpa = 12.0;
+        $isMale = true;
+    ?>
+
+
+</body>
+</html>
