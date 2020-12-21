dea445d969394e99272b9555ac8207158ef48e0e
LevanceWam <lkwamley@fullsail.edu>
2020-12-21 18:19:58 -0500

    Learned about variable in php forgot one part

--- HelloWorld/variables.php
+++ HelloWorld/variables.php
@@ -11,11 +11,14 @@
 <!-- we use a variable  -->
 <!-- we use the $ to define a varibale  -->
 <!-- to insert the value we need to place the variable with the $ where we want it  -->
+<!-- these variables can be modified through out the program -->
     <?php
     $characterName = "kevin";
     $age = 99;
     echo "there once was a man named $characterName <br>";
     echo "He was $age years old <br>";
+    // later on in the code we can change the value of the variable 
+    $characterName = "james";
     echo "He liked his name $characterName <br>";
     echo "He did not like being $age <br>";
     ?>
