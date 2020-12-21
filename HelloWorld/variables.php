7b53a1492ed2bbf409f3a71638a5d7c884c43aac
LevanceWam <lkwamley@fullsail.edu>
2020-12-21 18:06:31 -0500

    Learned about variables in php

---
+++ HelloWorld/variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- going to learn about variables in php -->
+<!-- now what if we wanted to change the name george but we only wanted to change it once   -->
+<!-- we use a variable  -->
+<!-- we use the $ to define a varibale  -->
+<!-- to insert the value we need to place the variable with the $ where we want it  -->
+    <?php
+    $characterName = "kevin";
+    $age = 99;
+    echo "there once was a man named $characterName <br>";
+    echo "He was $age years old <br>";
+    echo "He liked his name $characterName <br>";
+    echo "He did not like being $age <br>";
+    ?>
+</body>
+</html>
