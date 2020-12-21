67f0f62c62ba18537f4a3461bc26656166afa69e
LevanceWam <lkwamley@fullsail.edu>
2020-12-21 17:52:11 -0500

    create our first php file

---
+++ HelloWorld/site.php
@@ -0,0 +1,24 @@
+<!-- php is tightly coupled with html so if we understand html it will be ok -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <!-- we make a php tag and write php code -->
+    <!-- we can write any valid html in php -->
+    <!-- php executes the instructions in order  -->
+    <?php
+    // this command allows us to write information out to the html document 
+    // we are printing out to the html doecument 
+    // the semicolon is important because it lets php know we are done writing that line of code 
+        echo 'hello World';
+        echo '<hr>';
+        echo '<h1>Vance site</h1>'
+    ?>
+
+</body>
+</html>
