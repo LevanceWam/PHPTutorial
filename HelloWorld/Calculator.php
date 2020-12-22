662c82888e8449d1227f2c266ec02d5c8603d372
LevanceWam <lkwamley@fullsail.edu>
2020-12-21 21:32:29 -0500

    Built a simple calculator using php

---
+++ HelloWorld/Calculator.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!-- we are going to build a calculator using PHP and forms  -->
+<!-- first we need to get 2 numbers from the user -->
+<!-- add them together then return the sum -->
+    <form action="Calculator.php" method="get">
+        <input type="number" name="Num1">
+        <input type="number" name="Num2">
+        <input type="submit">
+    </form>
+
+    Your value is: <?php 
+    // storing the values from the form into the varible and eching the sum
+    // we didnt need the variables we can add the gets but it is good practice
+        $valueOne = $_GET['Num1'];
+        $valueTwo = $_GET['Num2'];
+        echo $valueOne + $valueTwo;
+    ?>
+</body>
+</html>
